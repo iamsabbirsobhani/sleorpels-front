30b0674b137700528e754a71d240cb733ed3a1c5
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-08-05 22:46:21 +0600

    Home Offer making beta

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  ignoreBuildErrors: true,
   swcMinify: true,
   images: {
     domains: ["images.asos-media.com"],
