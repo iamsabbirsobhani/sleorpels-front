2f78240d252491ac1af893cc5cab1a3254e1e74a
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-08-06 07:23:03 +0600

    Home page bug

---
+++ pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import PopulateOffer from "../components/home/homepage/PopulateOffer";
+
+export default function HomePage() {
+  return (
+    <>
+      <div>
+        <PopulateOffer />
+      </div>
+    </>
+  );
+}
