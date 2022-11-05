7d3691903ca0fd13f39d0a7e6a9110051545ca26
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-11-05 13:15:46 +0600

    Men loading state added

--- components/men/section-1/SalesBanner.tsx
+++ components/men/section-1/SalesBanner.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import { useEffect } from "react";
+import Head from "next/head";
+import loading from "../../../public/sleorpels-loading.png";
+import Loading from "../../Loading";
 
 export default function SalesBanner(props: any) {
   const { data } = props;
@@ -24,6 +27,50 @@ export default function SalesBanner(props: any) {
       });
     });
   });
+
+  // loading while data is fetching
+  if (!data)
+    return (
+      <>
+        <Head>
+          <title>Men's Clothes | Loading... | SLEORPELS</title>
+          <meta
+            name="description"
+            content="Buy all types of clothing with latest design and western vibe. Visit today and get many discounts."
+          />
+          <link rel="icon" href="/public/favicon.png" />
+
+          <meta
+            property="og:title"
+            content="Buy latest fashion with trusted brand"
+          />
+          <meta property="og:type" content="online products ecommerce" />
+          <meta
+            property="og:image"
+            content="https://firebasestorage.googleapis.com/v0/b/iceland-2b5ae.appspot.com/o/sleorpels-thumb.png?alt=media&token=5fb0783c-9980-4fee-bd1c-6699e2d63de9"
+          />
+          <meta
+            property="og:url"
+            content="https://firebasestorage.googleapis.com/v0/b/iceland-2b5ae.appspot.com/o/sleorpels-thumb.png?alt=media&token=5fb0783c-9980-4fee-bd1c-6699e2d63de9"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+
+        <div className=" h-[100vh] w-64 m-auto animate-pulse relative top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+          <div className=" absolute z-50 w-72 h-20 bg-transparent"></div>
+          <Image
+            className=" object-cover"
+            src={loading}
+            alt="sleorpels-loading"
+            width="500"
+            height="80"
+            blurDataURL={loading.src}
+            placeholder="empty"
+          />
+        </div>
+      </>
+    );
+
   return (
     <>
       {data && (
